b71556223a628d6b9bb7db269f3d366895a2973b
sean10776 <sean10776@gmail.com>
2024-03-05 13:30:11 +0800

    update

---
+++ app/src/main/java/com/ntougnsslab/l2homework/BMICalculator.kt
@@ -0,0 +1,14 @@
+package com.ntougnsslab.l2homework
+
+class BMICalculator {
+    fun analyzeBMI(userInfo: UserInfo?): /* TODO 補上回傳類型 */ {
+        /* TODO 要做null判斷，回傳錯誤 */
+
+        /* TODO 根據userInfo內的資料計算出BMI值 */
+        val bmi = "TODO: BMI值"
+        /* TODO 根據BMI值判斷出BMI結果，且BMI結果要根據男女還有年齡判斷 */
+        val bmiRes = "TODO: BMI結果"
+        /* TODO 把BMI值還有結果用Kotlin的format組合 */
+        return "TODO: 組合後的字串"
+    }
+}
---
+++ app/src/main/java/com/ntougnsslab/l2homework/UserInfo.kt
@@ -0,0 +1,10 @@
+package com.ntougnsslab.l2homework
+
+data class UserInfo(
+    /* TODO 請在這裡寫上屬性 (屬性名稱: 屬性型態 = 預設值) */
+    val name,
+    val gender,
+    val age,
+    val weight
+    val height
+)
